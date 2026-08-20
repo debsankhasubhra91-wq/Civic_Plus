cd38e47bef9218722608c66845b366a2b6bd870b
Sankha Subhra deb <debsankhasubhra91@gmail.com>
2026-08-21 00:00:54 +0530

    all files are added

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'src',
   base: './',
   plugins: [
     react(),
@@ -12,8 +13,13 @@ export default defineConfig({
     viteSingleFile(),
   ],
   build: {
+    outDir: '../',
+    emptyOutDir: false,
     target: 'esnext',
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html')
+    }
   }
 })
